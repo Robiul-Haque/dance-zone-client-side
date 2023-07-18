ba39da4db7f74ae3bfece75465abb16b93d7997b
Robiul-Haque <robiulcoc420@gmail.com>
2023-07-18 21:21:26 +0600

    create footer

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Shared/Footer/Footer";
+
+
+const Main = () => {
+    return (
+        <>
+          <Outlet></Outlet>
+          <Footer></Footer>
+        </>
+    );
+};
+
+export default Main;
